8516f3bf3d14a9b80a28251612f7c3ec20b107a7
williamaltu <williamaltu@70379056-a172-11de-a69d-91aafc7ba57a>
2009-09-28 15:39:48 +0000

    new order in zathura!!!!!

--- trunk/zathura-JavaEE-web-centric/src/co/edu/usbcali/lidis/zathura/generator/jeewebcentric/engine/IZathuraTemplate.java
+++ trunk/zathura-JavaEE-web-centric/src/co/edu/usbcali/lidis/zathura/generator/jeewebcentric/engine/IZathuraTemplate.java
@@ -1,4 +1,5 @@
 package co.edu.usbcali.lidis.zathura.generator.jeewebcentric.engine;
+
 /**
  * 
  * @author William Altuzarra Noriega
@@ -10,7 +11,7 @@ import co.edu.usbcali.lidis.zathura.metadata.model.MetaData;
 import co.edu.usbcali.lidis.zathura.metadata.model.MetaDataModel;
 
 public interface IZathuraTemplate {
-
+	
 	public void doTemplate(String hdLocation, MetaDataModel metaDataModel,
 			String jpaPckgName, String projectName, Integer specificityLevel);
 
@@ -40,5 +41,15 @@ public interface IZathuraTemplate {
 
 	public void doFacesConfig(MetaDataModel dataModel, VelocityContext context,
 			String hdLocation);
+
+	public void doDto(MetaData metaData, VelocityContext context,
+			String hdLocation, MetaDataModel dataModel, String modelName);
+
+	public void doExceptions(VelocityContext context, String hdLocation);
+
+	public void doUtilites(VelocityContext context, String hdLocation,
+			MetaDataModel dataModel, String modelName);
+
+	public void doJspFacelets(VelocityContext context, String hdLocation);
 
 }
