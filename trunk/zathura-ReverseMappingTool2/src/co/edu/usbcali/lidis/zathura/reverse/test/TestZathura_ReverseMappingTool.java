045d688523b1b77188cb844187af70d8d5ed445c
williamaltu <williamaltu@70379056-a172-11de-a69d-91aafc7ba57a>
2010-08-18 18:30:39 +0000

    Making capabilites for hibernate with XML !!!!!

--- trunk/zathura-ReverseMappingTool2/src/co/edu/usbcali/lidis/zathura/reverse/test/TestZathura_ReverseMappingTool.java
+++ trunk/zathura-ReverseMappingTool2/src/co/edu/usbcali/lidis/zathura/reverse/test/TestZathura_ReverseMappingTool.java
@@ -1,6 +1,5 @@
 package co.edu.usbcali.lidis.zathura.reverse.test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -13,43 +12,63 @@ public class TestZathura_ReverseMappingTool {
 	public static void main(String[] args) {
 		Properties connectionProperties = new Properties();
 
-//		String connectionDriverClass = "com.mysql.jdbc.Driver";
-//		String connectionUrl = "jdbc:mysql://localhost:3306/mascotas";
-//		String defaultSchema = "mascotas";
-//		String connectionUsername = "root";
-//		String connectionPassword = "root";
-//		String companyDomainName = "co.edu.usbcali.mascotas";
-//		String connectionDriverJarPath = "E:/WORKSPACE/mascotas/WebRoot/WEB-INF/lib/mysql-connector-java-3[1].1.12-bin.jar";
-//		String destinationDirectory = "E:/WORKSPACE/mascotas/src/";
-//		String matchSchemaForTables = null;
-//		List<String> tablesList = null;
-		
+		// String connectionDriverClass = "com.mysql.jdbc.Driver";
+		// String connectionUrl = "jdbc:mysql://localhost:3306/mascotas";
+		// String defaultSchema = "mascotas";
+		// String connectionUsername = "root";
+		// String connectionPassword = "root";
+		// String companyDomainName = "co.edu.usbcali.mascotas";
+		// String connectionDriverJarPath =
+		// "E:/WORKSPACE/mascotas/WebRoot/WEB-INF/lib/mysql-connector-java-3[1].1.12-bin.jar";
+		// String destinationDirectory = "E:/WORKSPACE/mascotas/src/";
+		// String matchSchemaForTables = null;
+		// List<String> tablesList = null;
+
+		// String connectionDriverClass = "com.mysql.jdbc.Driver";
+		// String connectionUrl = "jdbc:mysql://localhost:3306/tlpj";
+		// String defaultSchema = "tlpj";
+		// String connectionUsername = "root";
+		// String connectionPassword = "root";
+		// String companyDomainName = "com.tlp.tlpj";
+		// String connectionDriverJarPath =
+		// "E:/WORK(New)/tlpj/WebRoot/WEB-INF/lib/mysql-connector-java-5.1.5-bin";
+		// String connectionDriverJarPath =
+		// "E:/WORK(New)/tlpj/WebRoot/WEB-INF/lib/mysql-connector-java-3[1].1.12-bin.jar";
+		// String destinationDirectory = "E:/WORK(New)/tlpj/src/";
+		// String matchSchemaForTables = null;
+		// List<String> tablesList = null;
+		// Boolean makeItXml = false;
+
 		String connectionDriverClass = "oracle.jdbc.driver.OracleDriver";
 		String connectionUrl = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 		String defaultSchema = null;
-		String connectionUsername = "root";
+		String connectionUsername = "PA01";
 		String connectionPassword = "root";
-		String companyDomainName = "co.edu.usb.banco";
-		String connectionDriverJarPath = "E:/WORKSPACE/banco/WebRoot/WEB-INF/lib/ojdbc14.jar";
-		String destinationDirectory = "E:/WORKSPACE/banco/src/";
-		String matchSchemaForTables = "ROOT";
+		String companyDomainName = "org.cgiar.ciat.pa2";
+		String connectionDriverJarPath = "E:/WORK(New)/pa/WebRoot/WEB-INF/lib/ojdbc14.jar";
+		String destinationDirectory = "E:/WORK(New)/pa/src/";
+		String matchSchemaForTables = "PA01";
 		List<String> tablesList = ReverseUtil.fillTableList();
-		
+		Boolean makeItXml = false;
 
 		connectionProperties
 				.put("connectionDriverClass", connectionDriverClass);
 		connectionProperties.put("connectionUrl", connectionUrl);
-		connectionProperties.put("defaultSchema", defaultSchema == null ? "" : defaultSchema);
+		connectionProperties.put("defaultSchema", defaultSchema == null ? ""
+				: defaultSchema);
 		connectionProperties.put("connectionUsername", connectionUsername);
 		connectionProperties.put("connectionPassword", connectionPassword);
 		connectionProperties.put("companyDomainName", companyDomainName);
-		connectionProperties.put("matchSchemaForTables", matchSchemaForTables == null ? "" : matchSchemaForTables);
+		connectionProperties.put("matchSchemaForTables",
+				matchSchemaForTables == null ? "" : matchSchemaForTables);
 
 		connectionProperties.put("connectionDriverJarPath",
 				connectionDriverJarPath);
-		
-		connectionProperties.put("destinationDirectory",
-				destinationDirectory);
+
+		connectionProperties.put("destinationDirectory", destinationDirectory);
+
+		connectionProperties.put("makeItXml", makeItXml == true ? "True"
+				: false);
 
 		ReverseUtil.setFullPath(ReverseUtil.projectPathInConsole());
 
--- trunk/zathura-ReverseMappingTool2/src/co/edu/usbcali/lidis/zathura/reverse/utilities/ReverseUtil.java
+++ trunk/zathura-ReverseMappingTool2/src/co/edu/usbcali/lidis/zathura/reverse/utilities/ReverseUtil.java
@@ -14,10 +14,11 @@ public class ReverseUtil {
 			+ ReverseUtil.slash;
 	private static String tempFiles = "tempFiles" + ReverseUtil.slash;
 	private static String antRevEng = "antBuild-revEng" + ReverseUtil.slash;
-	
+
 	public static String getFullPath() {
 		return fullPath;
 	}
+
 	public static void setFullPath(String fullPath) {
 		ReverseUtil.fullPath = fullPath;
 	}
@@ -45,15 +46,15 @@ public class ReverseUtil {
 
 	public static String fixDomain(String domainName) {
 		String retFixDomian = null;
-		if(domainName == null){
+		if (domainName == null) {
 			return "";
 		}
-		if(domainName.length()>0){
+		if (domainName.length() > 0) {
 			retFixDomian = replaceAll(domainName, ".", ReverseUtil.slash);
 		}
 		return retFixDomian;
 	}
-	
+
 	/**
 	 * 
 	 * @param cadena
@@ -61,42 +62,42 @@ public class ReverseUtil {
 	 * @param snew
 	 * @return
 	 */
-	public static String replaceAll (String cadena,String old,String snew) {
-		StringBuffer replace= new StringBuffer();
+	public static String replaceAll(String cadena, String old, String snew) {
+		StringBuffer replace = new StringBuffer();
 		String aux;
-	
+
 		for (int i = 0; i < cadena.length(); i++) {
-			if ( (i+old.length()) <cadena.length() ){
-				aux =  cadena.substring(i,i+old.length());
-				if (aux.equals(old)){
+			if ((i + old.length()) < cadena.length()) {
+				aux = cadena.substring(i, i + old.length());
+				if (aux.equals(old)) {
 					replace.append(snew);
-					i+=old.length()-1;
-				}else{
-					replace.append(cadena.substring(i,i+1));
+					i += old.length() - 1;
+				} else {
+					replace.append(cadena.substring(i, i + 1));
 				}
-			}else
-				replace.append(cadena.substring(i,i+1));
+			} else
+				replace.append(cadena.substring(i, i + 1));
 		}
 		return replace.toString();
 	}
-	
-	public static String projectPathInConsole(){
+
+	public static String projectPathInConsole() {
 		URL url = ReverseUtil.class.getResource("ReverseUtil.class");
 		String classPath = url.getPath().substring(1);
-		int tmp = classPath.indexOf("zathura-ReverseMappingTool")+26;
-		
-		String inconmpletePath =  classPath.substring(0, tmp);
-		String cPath = ""+classPath.charAt(tmp);
+		int tmp = classPath.indexOf("zathura-ReverseMappingTool") + 26;
+
+		String inconmpletePath = classPath.substring(0, tmp);
+		String cPath = "" + classPath.charAt(tmp);
 		String projectPath = null;
-		
-		if(cPath.equals("2")){
-			projectPath = inconmpletePath+"2"+ReverseUtil.slash;
-		}else{
-			projectPath = inconmpletePath+ReverseUtil.slash;
-		}		
+
+		if (cPath.equals("2")) {
+			projectPath = inconmpletePath + "2" + ReverseUtil.slash;
+		} else {
+			projectPath = inconmpletePath + ReverseUtil.slash;
+		}
 		return projectPath;
 	}
-	
+
 	public static boolean validationsList(List list) {
 		if (list != null) {
 			if (!list.isEmpty() && list.size() > 0) {
@@ -108,19 +109,41 @@ public class ReverseUtil {
 			return false;
 		}
 	}
-	
-	public static List<String> fillTableList(){
+
+	public static List<String> fillTableList() {
 		List<String> tablesList = new ArrayList<String>();
-		
-		tablesList.add("CLIENTES");
-		tablesList.add("CONSIGNACIONES");
-		tablesList.add("CUENTAS");
-		tablesList.add("RETIROS");
-		tablesList.add("TIPOS_DOCUMENTOS");
-		tablesList.add("TIPOS_USUARIOS");
-		tablesList.add("USUARIOS");
-		
+
+		tablesList.add("TACHIEVEMENT");
+		tablesList.add("TAPPRAISAL");
+		tablesList.add("TAPPRAISALTYPE");
+		tablesList.add("TAPPROVAL");
+		tablesList.add("TCOMMITMENT");
+		tablesList.add("TCOMPETENCE");
+		tablesList.add("TCONTRIBUTION");
+		tablesList.add("TDEFINEDVALUES");
+		tablesList.add("TEVALUATOR");
+		tablesList.add("TFINALRATING");
+		tablesList.add("THEADER");
+		tablesList.add("TKNOWLEDGE");
+		tablesList.add("TKNOWLEDGEITEMS");
+		tablesList.add("TLANGUAGE");
+		tablesList.add("TLEARNINGNEED");
+		tablesList.add("TRATING");
+		tablesList.add("TRECOMENDATIONITEMS");
+		tablesList.add("TRECOMENDATIONS");
+		tablesList.add("TRESULTS");
+		tablesList.add("TSECTION");
+		//tablesList.add("TSECTIONTEXT");
+		tablesList.add("TSUBDEFINED");
+		tablesList.add("TTOOLITEMS");
+		tablesList.add("TTOOLS");
+
+		/*
+		TACHIEVEMENT, TAPPRAISAL , TAPPRAISALTYPE, TAPPROVAL, TCOMMITMENT, TCOMPETENCE, TCONTRIBUTION, TDEFINEDVALUES
+		TEVALUATOR, TFINALRATING, THEADER, TKNOWLEDGE, TKNOWLEDGEITEMS, TLANGUAGE, TLEARNINGNEED, TRATING, TRECOMENDATIONITEMS
+		TRECOMENDATIONS, TRESULTS, TSECTION, TSUBDEFINED, TTOOLITEMS, TTOOLS 
+		 
+*/
 		return tablesList;
 	}
-
 }
