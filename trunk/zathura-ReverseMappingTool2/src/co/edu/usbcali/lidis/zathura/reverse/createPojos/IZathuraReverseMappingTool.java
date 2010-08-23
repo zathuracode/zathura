642e69341322af23565b5264a112cfc238bd269b
williamaltu <williamaltu@70379056-a172-11de-a69d-91aafc7ba57a>
2010-08-23 17:22:09 +0000

    Another version in progress

--- trunk/zathura-ReverseMappingTool2/src/co/edu/usbcali/lidis/zathura/reverse/createPojos/IZathuraReverseMappingTool.java
+++
@@ -1,8 +0,0 @@
-package co.edu.usbcali.lidis.zathura.reverse.createPojos;
-
-import java.util.List;
-import java.util.Properties;
-
-public interface IZathuraReverseMappingTool {
-	public void makePojos(Properties connectionProperties, List<String> tables);
-}
