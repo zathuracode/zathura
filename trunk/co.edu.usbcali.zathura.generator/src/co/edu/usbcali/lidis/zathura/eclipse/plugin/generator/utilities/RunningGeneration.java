d5cde98187cb3b5fe0b7f52353ed5a7f64df30ba
dgomezusb <dgomezusb@70379056-a172-11de-a69d-91aafc7ba57a>
2011-03-23 01:38:34 +0000

    Mostrar mensajes de error en MessageDialog.openError(getShell(), "Error",e.getMessage());

--- trunk/co.edu.usbcali.zathura.generator/src/co/edu/usbcali/lidis/zathura/eclipse/plugin/generator/utilities/RunningGeneration.java
+++ trunk/co.edu.usbcali.zathura.generator/src/co/edu/usbcali/lidis/zathura/eclipse/plugin/generator/utilities/RunningGeneration.java
@@ -2,11 +2,14 @@ package co.edu.usbcali.lidis.zathura.eclipse.plugin.generator.utilities;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Shell;
 
 import co.edu.usbcali.lidis.zathura.generator.exceptions.GeneratorNotFoundException;
 import co.edu.usbcali.lidis.zathura.metadata.exceptions.MetaDataReaderNotFoundException;
@@ -19,13 +22,15 @@ import co.edu.usbcali.lidis.zathura.metadata.exceptions.MetaDataReaderNotFoundEx
  * @see IRunnableWithProgress
  */
 public class RunningGeneration implements IRunnableWithProgress {
+	
+	private org.eclipse.swt.widgets.Shell shell;
 
 	/**
 	 * RunningGeneration constructor
 	 * 
 	 */
-	public RunningGeneration() {
-
+	public RunningGeneration(Shell shell) {
+		this.shell=shell;
 	}
 
 	/**
@@ -42,24 +47,41 @@ public class RunningGeneration implements IRunnableWithProgress {
 
 			monitor.subTask("Refresh " + EclipseGeneratorUtil.projectName + " project...");
 			EclipseGeneratorUtil.project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+			
 			monitor.subTask("Building " + EclipseGeneratorUtil.projectName + " project...");
 			EclipseGeneratorUtil.project.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+			
+			//monitor.subTask("Building " + EclipseGeneratorUtil.projectName + " project...");
+			
+			
+			
 		} catch (MetaDataReaderNotFoundException e) {
 			monitor.setCanceled(true);
 			ZathuraGeneratorLog.logError(e);
+			MessageDialog.openError(getShell(), "Error",e.getMessage());
 			throw new InterruptedException(e.getMessage());
 		} catch (GeneratorNotFoundException e) {
 			monitor.setCanceled(true);
 			ZathuraGeneratorLog.logError(e);
+			MessageDialog.openError(getShell(), "Error",e.getMessage());
 			throw new InterruptedException(e.getMessage());
 		} catch (CoreException e) {
 			monitor.setCanceled(true);
 			ZathuraGeneratorLog.logError(e);
+			MessageDialog.openError(getShell(), "Error",e.getMessage());
 			throw new InterruptedException(e.getMessage());
 		}
 		monitor.done();
 		if (monitor.isCanceled()) {
 			throw new InterruptedException("The generation was cancelled");
 		}
+	}
+	
+	public org.eclipse.swt.widgets.Shell getShell() {
+		return shell;
+	}
+
+	public void setShell(org.eclipse.swt.widgets.Shell shell) {
+		this.shell = shell;
 	}
 }
