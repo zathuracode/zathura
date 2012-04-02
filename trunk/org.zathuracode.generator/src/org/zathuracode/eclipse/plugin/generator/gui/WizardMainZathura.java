10499e0b8d63fa08a3e63c97b03f7814097c485f
dgomez@vortexbird.com <dgomez@vortexbird.com@70379056-a172-11de-a69d-91aafc7ba57a>
2012-04-02 20:18:51 +0000

    Se puso el url del sitio web de zathuracode.org

--- trunk/org.zathuracode.generator/src/org/zathuracode/eclipse/plugin/generator/gui/WizardMainZathura.java
+++ trunk/org.zathuracode.generator/src/org/zathuracode/eclipse/plugin/generator/gui/WizardMainZathura.java
@@ -48,7 +48,7 @@ public class WizardMainZathura extends Wizard {
 	 */
 	public WizardMainZathura() {
 		super();
-		setWindowTitle("Zathuracode Generator V3.0 - Powered By Vortexbird www.vortexbird.com");
+		setWindowTitle("Zathuracode Generator V3.0 - Powered By Vortexbird www.zathuracode.org");
 		setDefaultPageImageDescriptor(ResourceManager.getPluginImageDescriptor(ZathuraGeneratorActivator.getDefault(), "icons/balvardi-Robotic7070.jpg"));
 		EclipseGeneratorUtil.reset();
 		EclipseGeneratorUtil.wizardMain=this;		
