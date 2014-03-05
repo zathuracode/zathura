8e62864977a4594e4a6db58efb5fdc0df9bb7a60
andrespuerta15 <andrespuerta15@70379056-a172-11de-a69d-91aafc7ba57a>
2014-03-05 04:09:15 +0000

    Autor: Andrés Puerta Descripción:Se implementan 2 metodos que se encargan de cargar las listas con las arquitecturas de cada version de zathura la 3.0 y la 3.1

--- org.zathuracode.generator/src/org/zathuracode/eclipse/plugin/generator/gui/WizardPageChooseGenerator.java
+++ org.zathuracode.generator/src/org/zathuracode/eclipse/plugin/generator/gui/WizardPageChooseGenerator.java
@@ -1,7 +1,7 @@
 package org.zathuracode.eclipse.plugin.generator.gui;
 
-import java.util.HashMap;
 
+import java.util.HashMap;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
@@ -9,10 +9,13 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.zathuracode.eclipse.plugin.generator.utilities.EclipseGeneratorUtil;
 import org.zathuracode.generator.factory.ZathuraGeneratorFactory;
 import org.zathuracode.generator.model.GeneratorModel;
+import org.zathuracode.generator.utilities.GeneratorUtil;
+import org.eclipse.jface.viewers.ComboViewer;
 
 
 // TODO: Auto-generated Javadoc
@@ -25,12 +28,20 @@ import org.zathuracode.generator.model.GeneratorModel;
  */
 public class WizardPageChooseGenerator extends WizardPage {
 
+	/** The list generators version 3.0  */
+	private List listGeneratorsVersion3_0;
+	
+	/** The list generators version 3.1 */
+	private List listGeneratorsVersion3_1;
+	
 	/** The list generators. */
 	private List listGenerators;
 	
 	/** The bwr description. */
 	private Browser bwrDescription;
-
+	
+	private ComboViewer comboViewer;
+	
 	// Load the zathura Generators names
 	/** The generators. */
 	private HashMap<String, GeneratorModel> theGenerators = ZathuraGeneratorFactory.getTheZathuraGenerators();
@@ -42,8 +53,6 @@ public class WizardPageChooseGenerator extends WizardPage {
 		super("wizardPage");
 		setTitle("Zathura Java Code Generator");
 		setDescription("Generate Java Application based on Open Standards and JavaEE Design Patterns");
-		// setImageDescriptor(ResourceManager.getPluginImageDescriptor(ZathuraGeneratorActivator.getDefault(),
-		// "icons/balvardi-Robotic7070.png"));
 		setPageComplete(false);
 	}
 
@@ -59,47 +68,109 @@ public class WizardPageChooseGenerator extends WizardPage {
 
 		final Group generatorChoiseGroup = new Group(container, SWT.NONE);
 		generatorChoiseGroup.setText("Choose Generator");
-		generatorChoiseGroup.setBounds(10, 10, 583, 281);
-
-		listGenerators = new List(generatorChoiseGroup, SWT.BORDER);
-		listGenerators.addSelectionListener(new SelectionAdapter() {
+		generatorChoiseGroup.setBounds(10, 10, 583, 400);
+		
+		final Label lblGeneratorVersion3_0 = new Label(generatorChoiseGroup, SWT.NONE);
+		lblGeneratorVersion3_0.setText("Old:");
+		lblGeneratorVersion3_0.setBounds(10, 23, 99, 17);
+		
+		listGeneratorsVersion3_0 = new List(generatorChoiseGroup, SWT.BORDER);
+		listGeneratorsVersion3_0.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
-				String architectureSelected = (listGenerators.getSelection()[0]);
+				listGeneratorsVersion3_1.deselectAll();
+				String architectureSelected = (listGeneratorsVersion3_0.getSelection()[0]);
 				String architectureName = ZathuraGeneratorFactory.getGeneratorNameForGuiName(architectureSelected);
 				EclipseGeneratorUtil.zathuraGeneratorName = architectureName;
 				GeneratorModel generatorModel = theGenerators.get(architectureName);
 				if (generatorModel != null) {
-					bwrDescription.setText(generatorModel.getDescription());
+					//Se reemplaza el texto ${RELATIVE_PATH} por la ruta relativa del proyecto,
+					//para que al momento de mostrar el html, se muestren las imagenes
+					//Andres Puerta
+					String descriptionHTML = generatorModel.getDescription().replace("${RELATIVE_PATH}", GeneratorUtil.getFullPath());					
+					bwrDescription.setText(descriptionHTML);
 					setPageComplete(true);
 				}
 			}
 		});
-		listGenerators.setBounds(10, 28, 203, 243);
+		listGeneratorsVersion3_0.setBounds(10, 43, 213, 80);
+		
+		
+		final Label lblGeneratorVersion3_1 = new Label(generatorChoiseGroup, SWT.NONE);
+		lblGeneratorVersion3_1.setText("New:");
+		lblGeneratorVersion3_1.setBounds(10, 130, 99, 17);
+		
+		listGeneratorsVersion3_1 = new List(generatorChoiseGroup, SWT.BORDER);
+		listGeneratorsVersion3_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				listGeneratorsVersion3_0.deselectAll();
+				String architectureSelected = (listGeneratorsVersion3_1.getSelection()[0]);
+				String architectureName = ZathuraGeneratorFactory.getGeneratorNameForGuiName(architectureSelected);
+				EclipseGeneratorUtil.zathuraGeneratorName = architectureName;
+				GeneratorModel generatorModel = theGenerators.get(architectureName);
+				if (generatorModel != null) {
+					//Se reemplaza el texto ${RELATIVE_PATH} por la ruta relativa del proyecto,
+					//para que al momento de mostrar el html, se muestren las imagenes
+					//Andres Puerta
+					String descriptionHTML = generatorModel.getDescription().replace("${RELATIVE_PATH}", GeneratorUtil.getFullPath());
+					bwrDescription.setText(descriptionHTML);
+					setPageComplete(true);
+				}
+			}
+		});
+		listGeneratorsVersion3_1.setBounds(10, 150, 213, 80);
 
 		bwrDescription = new Browser(generatorChoiseGroup, SWT.NONE);
-		bwrDescription.setBounds(219, 28, 354, 243);
-		loadListGenerators();
+		bwrDescription.setBounds(227, 23, 350, 370);
+
+		loadListGeneratorsVersion3_0();
+		loadListGeneratorsVersion3_1();
 	}
 
-	/**
-	 * Load list generators.
-	 */
-	public void loadListGenerators() {
-		if (listGenerators != null) {
-			listGenerators.removeAll();
-			for (GeneratorModel generatorModel : theGenerators.values()) {
-				if (EclipseGeneratorUtil.makeItXml == true && generatorModel.getPersistence().equals("hibernateCore") == true) {
-					listGenerators.add(generatorModel.getGuiName());
-				} else if (EclipseGeneratorUtil.makeItXml == false && generatorModel.getPersistence().equals("jpa") == true) {
-					listGenerators.add(generatorModel.getGuiName());
-				}
 
+	/**
+	 * Load list generators version 3.0.
+	 */
+	public void loadListGeneratorsVersion3_0() {
+		if (listGeneratorsVersion3_0 != null) {
+			listGeneratorsVersion3_0.removeAll();
+			for (GeneratorModel generatorModel : theGenerators.values()) {
+				if (generatorModel.getZathuraVersion().equals("3.0")) {
+					if (EclipseGeneratorUtil.makeItXml == true && generatorModel.getPersistence().equals("hibernateCore") == true) {
+						listGeneratorsVersion3_0.add(generatorModel.getGuiName());
+					} else if (EclipseGeneratorUtil.makeItXml == false && generatorModel.getPersistence().equals("jpa") == true) {
+						listGeneratorsVersion3_0.add(generatorModel.getGuiName());
+					}
+				}
 			}
 		}
+
 		if (bwrDescription != null) {
 			bwrDescription.setText("");
 		}
 	}
+	
+	/**
+	 * Load list generators version 3.1.
+	 */
+	public void loadListGeneratorsVersion3_1() {
+		if (listGeneratorsVersion3_1 != null) {
+			listGeneratorsVersion3_1.removeAll();
+			for (GeneratorModel generatorModel : theGenerators.values()) {
+				if (generatorModel.getZathuraVersion().equals("3.1")) {
+					if (EclipseGeneratorUtil.makeItXml == true && generatorModel.getPersistence().equals("hibernateCore") == true) {
+						listGeneratorsVersion3_1.add(generatorModel.getGuiName());
+					} else if (EclipseGeneratorUtil.makeItXml == false && generatorModel.getPersistence().equals("jpa") == true) {
+						listGeneratorsVersion3_1.add(generatorModel.getGuiName());
+					}
+				}
+			}
+		}
 
+		if (bwrDescription != null) {
+			bwrDescription.setText("");
+		}
+	}
+	
 }
